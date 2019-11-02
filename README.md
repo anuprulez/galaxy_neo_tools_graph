e75d516b3aae0b4a42c9810cdb5914fed908b4cb
Anup Kumar <anup.rulez@gmail.com>
2019-11-02 19:12:07 +0100

    Add python connection to graph database

--- README.md
+++ README.md
@@ -45,3 +45,6 @@ User account: neo4j/password
 
 Reading manual:
 https://neo4j.com/docs/developer-manual/3.2/introduction/
+
+
+conda create --name graph_db python=3.6
---
+++ execute_query.py
@@ -0,0 +1,12 @@
+from neo4j import GraphDatabase
+
+
+uri = "bolt://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
+
+def print_friends_of(tx, name):
+    for record in tx.run("MATCH (people:Person {name: {name}}) RETURN people.name LIMIT 10", name=name):
+        print(record["people.name"])
+
+with driver.session() as session:
+    session.read_transaction(print_friends_of, "Tom Hanks")
